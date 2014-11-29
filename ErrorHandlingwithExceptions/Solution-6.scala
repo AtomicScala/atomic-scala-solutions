9f388fca55637add5270b396f77d32aaba46cd18
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 12:18:18 -0700

    Solution 6 complete

--- ErrorHandlingwithExceptions/Solution-6.scala
+++ ErrorHandlingwithExceptions/Solution-6.scala
@@ -1,9 +1,34 @@
 // Solution-6.scala
 // Solution to Exercise 6 in "Error Handling with Exceptions"
+/* Create a class with two methods, f and g. In g, throw a new type of
+exception that you define. In f, call g, catch its exception and, in the catch
+clause, throw a different exception (of a second type that you define). Test
+your code. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-6.	Create a class with two methods, f and g. In g, throw a new type of exception that you define. In f, call g, catch its exception and, in the catch clause, throw a different exception (of a second type that you define). Test your code.
-{oldDescription}*/
+class Ex1 extends Exception
+class Ex2 extends Exception
 
-// needs solution
+class Solution6 {
+  def f = try {
+    g
+  } catch {
+    case e:Ex1 => throw new Ex2
+  }
+  def g = throw new Ex1
+}
+
+def t1 = try {
+  (new Solution6).g
+} catch {
+  case e:Ex1 => "Ex1"
+}
+
+def t2 = try {
+  (new Solution6).f
+} catch {
+  case e:Ex2 => "Ex2"
+}
+
+t1 is "Ex1"
+t2 is "Ex2"
