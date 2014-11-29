700c7a9fa7acef1325e4e6a092d8189d81a575bd
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 10:58:10 -0700

    Solution 2 complete

--- ErrorHandlingwithExceptions/Solution-2.scala
+++ ErrorHandlingwithExceptions/Solution-2.scala
@@ -1,9 +1,24 @@
 // Solution-2.scala
 // Solution to Exercise 2 in "Error Handling with Exceptions"
+/* Create a class with a simple method f. Create a var of that class and
+initialize it to the special pre-defined value null, which means “nothing.”
+Try to call f using this var. Now wrap the call in a try-catch clause to catch
+the exception. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-2.	Create a class with a simple method f. Create a var of that class and initialize it to null. Try to call f using this var. Now wrap the call in a try-catch clause to catch the exception.
-{oldDescription}*/
+class Solution2 {
+  def f = "Solution2"
+}
 
-// needs solution
+var s2:Solution2 = null
+
+// Produces java.lang.NullPointerException:
+// s2.f
+
+def t = try {
+  s2.f
+} catch {
+  case e:NullPointerException => "Throws null pointer exception"
+}
+
+t is "Throws null pointer exception"
