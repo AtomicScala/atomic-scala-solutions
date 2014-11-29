08ac678d533204c387842472258a648b40bd0bde
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 11:03:59 -0700

    Solution 3 Complete

--- ErrorHandlingwithExceptions/Solution-3.scala
+++ ErrorHandlingwithExceptions/Solution-3.scala
@@ -1,9 +1,20 @@
 // Solution-3.scala
 // Solution to Exercise 3 in "Error Handling with Exceptions"
+/* Create a Vector containing some elements. Try to index outside the range
+of that Vector. Now write code to catch the exception. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-3.	Create a Vector containing some elements. Try to index outside the range of that Vector. Now write code to catch the exception.
-{oldDescription}*/
+val v = Vector(1, 7, 22, 11, 17)
+println(v(4))
 
-// needs solution
+// Generates IndexOutOfBoundsException:
+// v(5)
+
+def t(i:Int) = try {
+  v(i)
+} catch {
+  case e:IndexOutOfBoundsException => "IndexOutOfBoundsException"
+}
+
+t(5) is "IndexOutOfBoundsException"
+t(-1) is "IndexOutOfBoundsException"
