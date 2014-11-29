d54722b539860b3f41d7e854118fac2eb33215ae
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 12:08:18 -0700

    Solution 5 complete

--- ErrorHandlingwithExceptions/Solution-5.scala
+++ ErrorHandlingwithExceptions/Solution-5.scala
@@ -1,9 +1,30 @@
 // Solution-5.scala
 // Solution to Exercise 5 in "Error Handling with Exceptions"
+/* Create three new subtypes of Exception. Write a method that throws all
+three. In another method, call the first method but only use a single catch
+clause that will catch all three types of exception. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-5.	Create three new subtypes of Exception. Write a method that throws all three. In another method, call the first method but only use a single catch clause that will catch all three types of exception.
-{oldDescription}*/
+class ex1 extends Exception
+class ex2 extends Exception
+class ex3 extends Exception
 
-// needs solution
+def throwall(n:Int) = n match {
+  case 1 => throw new ex1
+  case 2 => throw new ex2
+  case 3 => throw new ex3
+  case _ => "no throw"
+}
+
+def catchall(n:Int) = try {
+  throwall(n)
+} catch {
+  case e:ex1 => "ex1"
+  case e:ex2 => "ex2"
+  case e:ex3 => "ex3"
+}
+
+catchall(0) is "no throw"
+catchall(1) is "ex1"
+catchall(2) is "ex2"
+catchall(3) is "ex3"
