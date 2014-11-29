e85b9f7dc26815878000448e5eb459fbd592dac6
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 10:51:11 -0700

    Solution 1 complete

--- ErrorHandlingwithExceptions/Solution-1.scala
+++ ErrorHandlingwithExceptions/Solution-1.scala
@@ -1,9 +1,15 @@
 // Solution-1.scala
 // Solution to Exercise 1 in "Error Handling with Exceptions"
+/* Create a method that throws an object of class Exception inside a try
+block. Pass a String argument to the Exception constructor. Catch the
+exception inside a catch clause and print the String argument. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-1.	Create a method that throws an object of class Exception inside a try block. Pass the constructor for Exception a String argument. Catch the exception inside a catch clause and print the String argument.
-{oldDescription}*/
+def solution1 =
+  try {
+    throw new Exception("solution1")
+  } catch {
+    case err:Exception => err.getMessage
+  }
 
-// needs solution
+solution1 is "solution1"
