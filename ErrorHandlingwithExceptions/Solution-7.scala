aae635fb14fafeb8db592c2629838b765fe7fb22
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 13:23:53 -0700

    Solution 7 partially solved

--- ErrorHandlingwithExceptions/Solution-7.scala
+++ ErrorHandlingwithExceptions/Solution-7.scala
@@ -1,9 +1,50 @@
 // Solution-7.scala
 // Solution to Exercise 7 in "Error Handling with Exceptions"
+/* Demonstrate that a derived-class constructor cannot catch exceptions
+thrown by its base-class constructor. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-7.	Demonstrate that a derived-class constructor cannot catch exceptions thrown by its base-class constructor.
-{oldDescription}*/
+class Ex extends Exception
 
-// needs solution
+class Base1 {
+  println("Base1 constructor")
+  throw new Ex
+}
+
+class Derived1 extends Base1 {
+  println("Where do you put the try block?")
+}
+
+def t1 = try {
+  new Derived1
+} catch {
+  case e:Ex => "Ex"
+}
+
+t1 is "Ex"
+
+// Auxiliary constructors -- this part is incomplete!
+
+
+class Base2 {
+  def this(n:Int, i:Int) = {
+    this()
+    println("Base2 constructor")
+    throw new Ex
+  }
+}
+
+class Derived2 extends Base2 {
+  def this(n:Int) = {
+    super.this(n, 1)
+    this()
+  }
+}
+
+/*def t2 = try {
+  new Derived2
+} catch {
+  case e:Ex => "Ex"
+}
+
+t1 is "Ex"*/
