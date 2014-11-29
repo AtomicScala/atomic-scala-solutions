1407591b6e51927d6d55bf50f37a94b5a486a691
Bruce Eckel <BruceTEckel@gmail.com>
2014-11-29 11:56:07 -0700

    Solution 4 complete

--- ErrorHandlingwithExceptions/Solution-4.scala
+++ ErrorHandlingwithExceptions/Solution-4.scala
@@ -1,9 +1,19 @@
 // Solution-4.scala
 // Solution to Exercise 4 in "Error Handling with Exceptions"
+/* Inherit your own subclass of Exception. Write a constructor for this class
+that takes a String argument and stores it inside the base-class Exception
+object. Write a method that displays the stored String. Create a try-catch
+clause to test your new exception class. */
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}
-4.	Inherit your own subclass of Exception. Write a constructor for this class that takes a String argument and stores it inside the base-class Exception object. Write a method that displays the stored String. Create a try-catch clause to test your new exception class.
-{oldDescription}*/
+class FauxPas(msg:String) extends Exception(msg) {
+  def what = msg
+}
 
-// needs solution
+def t = try {
+  throw new FauxPas("Oops!")
+} catch {
+  case e:FauxPas => e.what
+}
+
+t is "Oops!"
