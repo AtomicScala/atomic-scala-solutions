38a3d411eb00ed84fe9f39085ef3973ac1e77ab0
BruceEckel <mindviewinc@gmail.com>
2014-12-02 16:29:29 -0700

    Added argparse and fixed simplify()

--- runner.bat
+++ runner.bat
@@ -7,13 +7,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--run", action='store_true', help="Run all the scala scripts and capture any errors")
-parser.add_argument("-s", "--simplify", action='store_true', help="Remove unimportant trace files")
+parser.add_argument("-s", "--simplify", action='store_true', help="Remove unimportant trace files & show non-empty error files")
 parser.add_argument("-c", "--clean", action='store_true', help="Remove all 'run' artifacts")
-args = parser.parse_args()
-if not any(vars(args).values()):
-    parser.print_help()
+parser.add_argument("-p", "--prerequisites", action='store_true', help="Compile prerequisites")
 
-####################################################################
 @contextmanager
 def visitDir(d):
     os.chdir(d)
@@ -22,8 +19,28 @@ def visitDir(d):
 
 paths = [os.path.join('.', p[0:-1]) for p in glob('*/')]
 
-####################################################################
+compileFiles = [
+    # (Directory, [(file, artifact), (file, artifact), ...])
+    ("ImportsAndPackages-2ndEdition", [
+        ("EquilateralTriangle.scala", "com/atomicscala/pythagorean/EquilateralTriangle.class"),
+        ("PythagoreanTheorem.scala", "com/atomicscala/pythagorean/RightTriangle.class")
+    ]),
+]
+
+
+def compilePrerequisites():
+    print "Compiling prerequisites"
+    for direct, items in compileFiles:
+        with visitDir(direct):
+            for scala, dep in items:
+                if not os.path.exists(dep):
+                    cmd = "scalac " + scala
+                    print direct + ":", cmd
+                    os.system(cmd)
+
+
 def run():
+    print "Running"
     for p in paths:
         print p
         with visitDir(p):
@@ -33,36 +50,41 @@ def run():
                 print "    " + cmd
                 os.system(cmd)
 
-if args.run:
-    run()
 
-####################################################################
 def simplify():
+    print "Simplifying"
     for p in paths:
         with visitDir(p):
-            logs = glob(".out") + glob(".err") + glob("_AtomicTestErrors.txt")
+            print os.path.basename(p)
+            logs = glob("*.out") + glob("*.err") + glob("_AtomicTestErrors.txt")
             if "_AtomicTestErrors.txt" in "".join(logs):
-                print p + " contains _AtomicTestErrors.txt"
+                # Save the output files, only erase empty .err files
+                print "   _AtomicTestErrors.txt"
                 for e in [f for f in logs if f.endswith(".err")]:
                     if not os.path.getsize(e):
-                        print "removing " + os.path.basename(e)
+                        print "    removing " + os.path.basename(e)
                         os.remove(e)
             else:
                 for e in [f for f in logs if f.endswith(".err")]:
+                    print "  ", e, os.path.getsize(e)
                     if not os.path.getsize(e):
-                        print "removing " + os.path.basename(e) + " and " + os.path.splitext(e)[0] + ".out"
+                        # Remove .out and .err when .err is empty
+                        print "    removing " + os.path.basename(e) + " and " + os.path.splitext(e)[0] + ".out"
                         os.remove(e)
                         os.remove(os.path.splitext(e)[0] + ".out")
 
-if args.simplify:
-    simplify()
 
-####################################################################
 def clean():
+    print "Cleaning"
     removals = [os.path.join(d[0], f) for d in os.walk(".") for f in d[2]
                 if f.endswith(".out") or
                 f.endswith(".err") or
                 f == "_AtomicTestErrors.txt"]
 
-if args.clean:
-    clean()
+
+args = parser.parse_args()
+if not any(vars(args).values()): parser.print_help()
+if args.clean: clean() # Happens first with multiple command args
+if args.prerequisites: compilePrerequisites()
+if args.run: run()
+if args.simplify: simplify()
