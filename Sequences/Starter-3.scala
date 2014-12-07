58f3b7346f375aeed4c83ff63d33f00953321ee8
BruceEckel <mindviewinc@gmail.com>
2014-12-07 15:36:01 -0700

    Fixed per bug report
    https://github.com/AtomicScala/atomic-scala-corrections/issues/12

--- Sequences/Starter-3.scala
+++ Sequences/Starter-3.scala
@@ -10,6 +10,6 @@ val people = Vector(
 val sorted = // call sort here
 
 sorted is "Vector(" +
-+ "Person(Mary,Add,mary@add.com)," +
-+ "Person(Zach,Smith,zach@smith.com)," +
-+ "Person(Sally,Taylor,sally@taylor.com))"
+  "Person(Mary,Add,mary@add.com)," +
+  "Person(Zach,Smith,zach@smith.com)," +
+  "Person(Sally,Taylor,sally@taylor.com))"
