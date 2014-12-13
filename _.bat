0330921cdca825a41e87764a6431e37a276c1825
BruceEckel <mindviewinc@gmail.com>
2014-12-13 09:23:12 -0700

    msg for end users

--- _.bat
+++ _.bat
@@ -1,1 +1,9 @@
+@echo off
 start "atomic scala solutions" run.bat
+if ERRORLEVEL 1 (
+    echo.
+    echo =================================================
+    echo This command is part of the tools used to create
+    echo and test these solutions. You can ignore it.
+    echo =================================================
+)
