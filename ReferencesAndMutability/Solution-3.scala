ac190b399b4ee7f6180270936891ebde8514b2ba
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-02 18:48:19 -0800

    Working on List

--- ReferencesAndMutability/Solution-3.scala
+++ ReferencesAndMutability/Solution-3.scala
@@ -1,29 +1,23 @@
 // Solution-3.scala
 // Solution to Exercise 3 in "References & Mutability"
 import com.atomicscala.AtomicTest._
-
-/*var a = Array(1,2,3)
-val original = a
-a(1) = 44
-a :+= 17
-println(a.mkString(" "))
-println(original.mkString(" "))*/
-var lst = List(1,2,3)
+import collection.mutable.MutableList
+var lst = MutableList(1,2,3)
+val original = lst
 
 println(lst)
 
-def testSeq(s:Seq[Int]) = {
-  val x = s :+ 11
-  println(x)
-  val y = s ++ s
-  println(y)
-  val z = s ++: s
-  println(z)
-  //s :+= 99
-}
-
-//testSeq(a)
-//testSeq(lst)
+val x = lst :+ 11
+println(x)
+val y = lst ++ lst
+println(y)
+val z = lst ++: lst
+println(z)
+lst :+= 99
+println(lst)
+println(original)
+original += 99
+println(original)
 
 /* OUTPUT_SHOULD_BE
 
