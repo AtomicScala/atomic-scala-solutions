fa08c7e3c5313ef87025479a178bec90e6ef64c8
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-03 17:43:27 -0800

    Progress on solution 3

---
+++ ReferencesAndMutability/Solution-3a.scala
@@ -0,0 +1,27 @@
+// Solution-3a.scala
+// Solution to Exercise 3 in "References & Mutability"
+import com.atomicscala.AtomicTest._
+
+// The default List is immutable
+
+var lst = List(1,2,3)
+val original = lst
+// You can also create a list like this:
+val colors = "red" :: "green" :: "yellow" :: List()
+colors is "List(red, green, yellow)"
+
+lst :+= 99 // Now lst is pointing at a different List
+lst is "List(1, 2, 3, 99)"
+lst == original is false
+val original2 = lst
+lst +:= -1
+lst is "List(-1, 1, 2, 3, 99)"
+lst == original2 is false
+
+lst(4) is 99
+// lst(2) = 111 // Can't modify elements; it's immutable
+lst is "List(-1, 1, 2, 3, 99)"
+
+/* OUTPUT_SHOULD_BE
+
+*/
---
+++ ReferencesAndMutability/Solution-3b.scala
@@ -0,0 +1,37 @@
+// Solution-3b.scala
+// Solution to Exercise 3 in "References & Mutability"
+import com.atomicscala.AtomicTest._
+import collection.mutable.MutableList
+
+// Unlike Map and Set, importing MutableList
+// does NOT make the default List mutable:
+
+val notMutable = List('a', 'b', 'c')
+notMutable.getClass is "class scala.collection.immutable.$colon$colon"
+
+// For a mutable List you must explitly ask for one:
+
+val yesItsMutable = MutableList('a', 'b', 'c')
+yesItsMutable.getClass is "class scala.collection.mutable.MutableList"
+
+// You can't do the colon trick with a MutableList:
+// val colors = "red" :: "green" :: "yellow" :: MutableList()
+
+var lst = MutableList(1,2,3)
+val original = lst
+
+lst :+= 99
+lst is "MutableList(1, 2, 3, 99)"
+lst == original is false
+val original2 = lst
+lst +:= -1
+lst is "MutableList(-1, 1, 2, 3, 99)"
+lst == original2 is false
+
+lst(4) is 99
+lst(4) = 111 // You can modify elements
+lst is "MutableList(-1, 1, 2, 3, 111)"
+
+/* OUTPUT_SHOULD_BE
+
+*/
