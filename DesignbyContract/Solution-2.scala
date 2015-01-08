5ff093d5b8392e0427ddb8a300c3ca4a30b4db89
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-07 20:21:13 -0800

    Solution 2 Completed

--- DesignbyContract/Solution-2.scala
+++ DesignbyContract/Solution-2.scala
@@ -3,19 +3,14 @@
 // Compile, then run with
 // > scala ConvertLetters Eleven pigs named FunkleBudder had8 part%es
 
-/*{oldDescription} 2.  Write an App (see Applications) with a method
-that takes the command-line argument of a String of letters,
-converts it to lowercase, and then converts each character to its
-numerical value in the alphabet, with 'a' being 1, 'b' being 2, etc.
-Sum the values and display the result. Use preconditions to verify
-that the input is in the correct form, and postconditions to ensure
-that the result is in the expected range of values.
-{oldDescription}*/
-
 object ConvertLetters extends App {
   def convert(s:String) = {
     require(s.forall(_.isLetter), "Only letters are allowed")
     val converted = s.toLowerCase.map(_.asDigit - 'a'.asDigit + 1).reduce(_ + _)
+    val lowerBound = s.length * 1
+    assume(converted >= lowerBound, "Result should be >= " + lowerBound)
+    val upperBound = s.length * ('z'.asDigit + 1)
+    assume(converted <= upperBound, "Result should be <= " + upperBound)
     println(converted)
   }
   for(arg <- args)
