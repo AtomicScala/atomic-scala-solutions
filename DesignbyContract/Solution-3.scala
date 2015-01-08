a7d92f0795e62bef90d169f76d689f51b34fccf5
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-07 23:21:38 -0800

    Partway through 3

--- DesignbyContract/Solution-3.scala
+++ DesignbyContract/Solution-3.scala
@@ -2,14 +2,23 @@
 // Solution to Exercise 3 in "Design by Contract"
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription} 3.  Write a class with a method that only has a
-postcondition that fails when you give the method particular inputs.
-Elide the postcondition and show that the failure then slips
-through. Add a precondition to prevent the failure.
-{oldDescription}*/
+/* Write a class with a method that only has a postcondition that fails when you
+give the method particular inputs. Elide the postcondition and show that the
+failure then slips through. Add a precondition to prevent the failure.*/
 
-// needs solution
+def f(n:Int) = {
+  assume(n % 2 != 0, "n is even: " + n)
+}
 
+def test(nums:Int*) =
+  for(n <- nums)
+    try {
+      f(n)
+    } catch {
+      case e:Throwable => println(e.getMessage)
+    }
+
+test(1, 4, 5, 7, 11, 22, 23)
 /* OUTPUT_SHOULD_BE
 
 */
