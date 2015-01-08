3fb6a383d1c470294c0e94aa5cc1fd1d3608ffe8
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-07 17:11:28 -0800

    Solution 1 completed

--- DesignbyContract/Solution-1.scala
+++ DesignbyContract/Solution-1.scala
@@ -1,17 +1,78 @@
-// Solution-1.scala Solution to Exercise 1 in "Design by Contract"
+// Solution-1.scala
+// Solution to Exercise 1 in "Design by Contract"
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription}  1.  Create a class with three methods: the first
-checks only preconditions, the second checks only postconditions,
-and the third checks both. Each method has the same body: it takes a
-String argument which must be between 4-10 characters, and each of
-those characters must represent a digit. Each method converts each
-digit into an Int and then adds up all the digits to produce the
-result. The postcondition should check the result to verify that it
-is in the expected range of values. {oldDescription}*/
+/* Note: foldLeft is more elegant and succinct and doesn't require
+   a var. However, you could also have said:
+    var sum = 0
+    digits.foreach(sum += _.asDigit)
+    sum
+*/
 
-// needs solution
+def f1(digits:String) = {
+  require(digits.length >= 4, "Need 4 or more digits")
+  require(digits.length <= 10, "No more than 10 digits allowed")
+  require(digits.forall(_.isDigit), "Only digits are allowed")
+  digits.foldLeft(0)(_ + _.asDigit)
+  // By inspection, the preconditions guarantee
+  // the postconditions.
+}
+
+def f2(digits:String) = {
+  // Note that in this example, without the preconditions
+  // the postconditions cannot be insured:
+  var result = digits.foldLeft(0)(_ + _.asDigit)
+  assume(result >= 0, "Result should not be negative")
+  assume(result <= 90, "Result cannot be greater than 90")
+  result
+}
+
+def f3(digits:String) = {
+  require(digits.length >= 4, "Need 4 or more digits")
+  require(digits.length <= 10, "No more than 10 digits allowed")
+  require(digits.forall(_.isDigit), "Only digits are allowed")
+  var result = digits.foldLeft(0)(_ + _.asDigit)
+  assume(result >= 0, "Result should not be negative")
+  assume(result <= 90, "Result cannot be greater than 90")
+  result
+}
+
+def test(funs: String => Int*) = {
+  def tst(fun: String => Int, digits:String, expected:Int) =
+    try {
+      fun(digits) is expected
+    } catch {
+      case e:Throwable => println(e.getMessage)
+    }
+  for(fun <- funs) {
+    tst(fun, "0000", 0)
+    tst(fun, "9999999999", 90)
+    tst(fun, "000", 0)
+    tst(fun, "99999999999", 90)
+    tst(fun, "9a999999", 73)
+    println("------------------")
+  }
+}
+
+test(f1, f2, f3)
 
 /* OUTPUT_SHOULD_BE
-
+0
+90
+requirement failed: Need 4 or more digits
+requirement failed: No more than 10 digits allowed
+requirement failed: Only digits are allowed
+------------------
+0
+90
+0
+assumption failed: Result cannot be greater than 90
+73
+------------------
+0
+90
+requirement failed: Need 4 or more digits
+requirement failed: No more than 10 digits allowed
+requirement failed: Only digits are allowed
+------------------
 */
