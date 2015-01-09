06b63c06e32a5fcf56f048a097bd3ebe37118581
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-08 21:24:12 -0800

    Exercise 2 solved

--- ExtensionMethods/Solution-2.scala
+++ ExtensionMethods/Solution-2.scala
@@ -2,16 +2,14 @@
 // Solution to Exercise 2 in "Extension Methods"
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription} 2.  Modify ExtensionMethodArguments.scala by adding an
-additional extension method in that has two arguments. Write tests.
-{oldDescription}*/
-
 case class Book(title:String)
 
 object BookExtension {
   implicit class Ops(book:Book) {
     def categorize(category:String) =
       s"$book, category: $category"
+    def ISBN(category:String, ISBN:String) =
+      s"${categorize(category)}, ISBN:$ISBN"
   }
 }
 import BookExtension._
@@ -19,6 +17,10 @@ import BookExtension._
 Book("Dracula") categorize "Vampire" is
 "Book(Dracula), category: Vampire"
 
-/* OUTPUT_SHOULD_BE
+Book("The Cider House Rules").ISBN("Mainstream Fiction", "123-456-7890") is
+"Book(The Cider House Rules), category: Mainstream Fiction, ISBN:123-456-7890"
 
+/* OUTPUT_SHOULD_BE
+Book(Dracula), category: Vampire
+Book(The Cider House Rules), category: Mainstream Fiction, ISBN:123-456-7890
 */
