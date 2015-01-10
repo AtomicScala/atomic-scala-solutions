1bc60d64a5e04d4bfc94b45ea5f571c520edfc7e
Bruce Eckel <BruceTEckel@gmail.com>
2015-01-10 08:33:38 -0800

    Solution 5 solved

--- ExtensibleSystemswithTypeClasses/Solution-5.scala
+++ ExtensibleSystemswithTypeClasses/Solution-5.scala
@@ -2,16 +2,45 @@
 // Solution to Exercise 5 in "Extensible Systems with Type Classes"
 import com.atomicscala.AtomicTest._
 
-/*{oldDescription} 5.  Create a type class trait called Reporter
-with a method generate. Write a method report that takes any object
-and its associated Reporter and produces a String (using generate)
-containing information about that object. Create case classes
-Person, Store and Vehicle, each containing different types of
-information. Create their associated Reporter objects and show that
-your type class system works correctly. {oldDescription}*/
+trait Reporter[I] {
+  def generate(info:I):String
+}
 
-// needs solution
+def report[I](info:I)(implicit reporter:Reporter[I]) =
+  reporter.generate(info)
+
+case class Person(name:String, age:Int, occupation:String)
+
+implicit object PersonReporter extends Reporter[Person] {
+  def generate(person:Person) =
+    s"${person.name}, age: ${person.age}, occupation: ${person.occupation}"
+}
+
+case class Store(name:String, location:String, products:Vector[String])
+
+implicit object StoreReporter extends Reporter[Store] {
+  def generate(store:Store) =
+    s"${store.name}, location: ${store.location}, products: ${store.products}"
+}
+
+case class Vehicle(manufacturer:String, make:String, model:String, year:Int)
+
+implicit object VehicleReporter extends Reporter[Vehicle] {
+  def generate(vehicle:Vehicle) =
+    s"${vehicle.manufacturer}, make: ${vehicle.make}, model: ${vehicle.model}, year: ${vehicle.year}"
+}
+
+report(Person("Arnold Toynbee", 30, "Economic Historian")) is
+  "Arnold Toynbee, age: 30, occupation: Economic Historian"
+
+report(Store("Alf's Drugs", "4th and Main", Vector("Drugs", "Candy", "Sundries"))) is
+  "Alf's Drugs, location: 4th and Main, products: Vector(Drugs, Candy, Sundries)"
+
+report(Vehicle("Morris Garage", "MG", "B GT", 1968)) is
+  "Morris Garage, make: MG, model: B GT, year: 1968"
 
 /* OUTPUT_SHOULD_BE
-
+Arnold Toynbee, age: 30, occupation: Economic Historian
+Alf's Drugs, location: 4th and Main, products: Vector(Drugs, Candy, Sundries)
+Morris Garage, make: MG, model: B GT, year: 1968
 */
